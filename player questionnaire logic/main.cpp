d9f525d8d72ac72313c0a860a3584a8ac342c38e
abyss of harmony <denysshpoltakov@gmail.com>
2026-04-18 13:20:46 +0200

    feat: add player profile input system

---
+++ player questionnaire logic/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string getPlayerName = "";
+    int getPlayerAge;
+    cout << "Enter your name: ";
+    cin >> getPlayerName;
+    cout << "Enter your age: ";
+    cin >> getPlayerAge;
+    cout << "Your name is: " << getPlayerName << " and your age is: " << getPlayerAge; 
+}

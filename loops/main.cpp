f59cec185062f5eb8e0f6b18e8d601c2e25ef166
abyss of harmony <denysshpoltakov@gmail.com>
2026-04-18 20:21:07 +0200

    add my danger scanner using for-loop and arrays

--- loops/main.cpp
+++ loops/main.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main() {
     string items[3] = {"Medkit", "Pistol", "Grenade"};
+    int enemies[5] = {10, 45, 90, 15, 120};
     for (int i = 10; i != 0; i--) {
         cout << i << endl;
     }
     cout << "BOOM!" << endl;
     for (int i = 0; i < 3; i++) {
-        cout << items[i] << " ";
+        cout << items[i] << " " << endl;
+    }
+    for (int i = 0; i < 5; i++) {
+        if (enemies[i] > 80) {
+            cout << "Enemy: " << i << ": CRITICAL DANGER!" << endl;
+        } else {
+            cout << "Enemy: " << i << ": Normal" << endl;
+        }
     }
 }

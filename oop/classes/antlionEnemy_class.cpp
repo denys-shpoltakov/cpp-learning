2c140a24ef539ebecfc9a114d8475d74a1c03084
abyss of harmony <denysshpoltakov@gmail.com>
2026-04-26 14:14:55 +0200

    solve task creating class, constructor and takeDamage method. Also write condition if hp < 0 do m_health = 0

---
+++ oop/classes/antlionEnemy_class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Antlion {
+public:
+    int m_health;
+    string m_type;
+
+    Antlion(int health, string type) : m_health(health), m_type(type) {};
+
+    void takeDamage(int amount) {
+        m_health -= amount;
+        if (m_health <= 0) {
+            m_health = 0;
+            cout << "Antlion: " << m_type << " is dead" << " current HP of antlion: " << m_health << endl;
+        }
+    }
+};
+
+int main() {
+    Antlion lion(100, "Soldier");
+    lion.takeDamage(101);
+}
